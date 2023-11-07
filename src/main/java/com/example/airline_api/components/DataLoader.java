d609f2fd571b4283ff5d699e884610d74f781688
Suzi Towers <suzitowers@gmail.com>
2023-11-07 16:53:01 +0000

    add data to dataloader

--- src/main/java/com/example/airline_api/components/DataLoader.java
+++ src/main/java/com/example/airline_api/components/DataLoader.java
@@ -1,5 +1,7 @@
 package com.example.airline_api.components;
 
+import com.example.airline_api.models.Flight;
+import com.example.airline_api.models.Passenger;
 import com.example.airline_api.repositories.FlightRepository;
 import com.example.airline_api.repositories.PassengerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,22 @@ public class DataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception{
+        // Create and save flights
+        Flight flight1 = new Flight("Baku", 200, "2023-12-01", "10:00 AM");
+        Flight flight2 = new Flight("Tokyo", 150, "2023-12-02", "11:00 AM");
+        flightRepository.save(flight1);
+        flightRepository.save(flight2);
 
+        // Create and save passengers
+        Passenger passenger1 = new Passenger("Suzi", "suzi@gamil.com");
+        Passenger passenger2 = new Passenger("Mark", "mark@gamil.com");
+        passengerRepository.save(passenger1);
+        passengerRepository.save(passenger2);
+
+        // Book passengers on flights
+        flight1.getPassengers().add(passenger1);
+        flight2.getPassengers().add(passenger2);
+        flightRepository.save(flight1);
+        flightRepository.save(flight2);
     }
 }
