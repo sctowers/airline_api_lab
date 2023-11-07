47ef1fc6af54cc255b924f538530110bfbd07532
Suzi Towers <suzitowers@gmail.com>
2023-11-07 20:23:02 +0000

    add exception to prevent passenger to be added to a full flight

--- src/main/java/com/example/airline_api/services/FlightService.java
+++ src/main/java/com/example/airline_api/services/FlightService.java
@@ -7,7 +7,9 @@ import com.example.airline_api.repositories.PassengerRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -41,13 +43,28 @@ public class FlightService {
     public Flight addPassengerToFlight(Long flightId, Long passengerId) {
         Optional<Flight> flightOptional = flightRepository.findById(flightId);
         Optional<Passenger> passengerOptional = passengerRepository.findById(passengerId);
-        Flight flight = flightOptional.get();
-        Passenger passenger = passengerOptional.get();
-        flight.getPassengers().add(passenger);
-        flightRepository.save(flight);
-        return flight;
-    }
 
+        if (flightOptional.isPresent() && passengerOptional.isPresent()){
+            Flight flight = flightOptional.get();
+            Passenger passenger = passengerOptional.get();
+
+            // Check if the flight is not full
+            if (flight.getPassengers().size() < flight.getCapacity()){
+                // Check if the passenger is not already booked on the flight
+                if (!flight.getPassengers().contains(passenger)){
+                    flight.getPassengers().add(passenger);
+                    flightRepository.save(flight);
+                    return flight;
+                } else {
+                    throw new ResponseStatusException(HttpStatus.CONFLICT, "Passenger is already booked on this flight");
+                }
+            } else {
+                throw new ResponseStatusException(HttpStatus.CONFLICT, "The flight is already full");
+            }
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Flight or passenger not found");
+        }
+    }
 
     // Cancel a flight
     @Transactional
