23be856daa4150c9965801d594bb38bdf74189cb
Suzi Towers <suzitowers@gmail.com>
2023-11-07 15:48:56 +0000

    add Dataloader and controllers package

--- src/main/java/com/example/airline_api/components/DataLoader.java
+++ src/main/java/com/example/airline_api/components/DataLoader.java
@@ -1,4 +1,27 @@
 package com.example.airline_api.components;
 
-public class DataLoader {
+import com.example.airline_api.repositories.FlightRepository;
+import com.example.airline_api.repositories.PassengerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    FlightRepository flightRepository;
+
+    @Autowired
+    PassengerRepository passengerRepository;
+
+    public DataLoader() {
+
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception{
+        
+    }
 }
--- src/main/java/com/example/airline_api/services/FlightServices.java
+++ src/main/java/com/example/airline_api/services/FlightServices.java
@@ -1,4 +1,14 @@
 package com.example.airline_api.services;
 
+import com.example.airline_api.repositories.FlightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FlightServices {
+
+    @Autowired
+    FlightRepository flightRepository;
+
+
 }
