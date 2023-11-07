9bf0b3871968ee9419286716ef52843b15958b9c
Suzi Towers <suzitowers@gmail.com>
2023-11-07 16:44:24 +0000

    add passengerServices methods

--- src/main/java/com/example/airline_api/services/PassengerService.java
+++ src/main/java/com/example/airline_api/services/PassengerService.java
@@ -1,8 +1,11 @@
 package com.example.airline_api.services;
 
+import com.example.airline_api.models.Passenger;
 import com.example.airline_api.repositories.PassengerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PassengerService {
@@ -10,5 +13,20 @@ public class PassengerService {
     @Autowired
     PassengerRepository passengerRepository;
 
+
+    // Get all details of passengers
+    public List<Passenger> getAllPassengers() {
+        return passengerRepository.findAll();
+    }
+
+    // Get details of a specific passenger
+    public Passenger getPassengerById(Long id){
+        return passengerRepository.findById(id).get();
+    }
+
+    // Add a new passenger
+    public Passenger addPassenger(Passenger passenger){
+        return passengerRepository.save(passenger);
+    }
 
 }
